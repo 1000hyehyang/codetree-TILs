4de9697e5475299fa690aef4673cca9a93e6332d
1000hyehyang <102294782+1000hyehyang@users.noreply.github.com>
2024-01-25 10:40:41 +0900

    Title: 최고의 33위치, Time: 117ms, Memory: 25MB, Status: Passed - CodeTree

--- 240125/최고의 33위치/best-place-of-33.py
+++ 240125/최고의 33위치/best-place-of-33.py
@@ -1,28 +1,32 @@
 n = int(input())
-
-matrix = [
+grid = [
     list(map(int, input().split()))
     for _ in range(n)
 ]
 
-def coin(row_s, col_s, row_e, col_e):
-    coins = 0
 
-    for row in range(col_s, col_e + 1):
+# (row_s, col_s) ~ (row_e, col_e) 사이의 금의 개수를 계산합니다.
+def get_num_of_gold(row_s, col_s, row_e, col_e):
+    num_of_gold = 0
+
+    for row in range(row_s, row_e + 1):
         for col in range(col_s, col_e + 1):
-            coins += matrix[row][col]
-        
-    return coins
-    
-max_coin = 0
+            num_of_gold += grid[row][col]
 
+    return num_of_gold
+
+
+max_gold = 0
+# (row, col)이 3 * 3 격자의 좌측 상단 모서리인 경우를 전부 탐색합니다. 
 for row in range(n):
     for col in range(n):
+        # 3 * 3 격자가 n * n 격자를 벗어나는 경우는 계산하지 않습니다.
         if row + 2 >= n or col + 2 >= n:
             continue
         
-        num_of_coin = coin(row, col, row + 2, col + 2)
+        num_of_gold = get_num_of_gold(row, col, row + 2, col + 2)
+            
+        # 최대 금의 개수를 저장합니다.
+        max_gold = max(max_gold, num_of_gold)
 
-        max_coin = max(max_coin, num_of_coin)
-
-print(max_coin)
+print(max_gold)
