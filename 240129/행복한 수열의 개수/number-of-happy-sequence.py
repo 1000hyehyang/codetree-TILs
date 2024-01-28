d284679c28294eb060d7ad7424346244d472f4da
1000hyehyang <102294782+1000hyehyang@users.noreply.github.com>
2024-01-29 05:00:44 +0900

    Title: 행복한 수열의 개수, Time: 128ms, Memory: 25MB, Status: Passed - CodeTree

--- 240129/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240129/행복한 수열의 개수/number-of-happy-sequence.py
@@ -10,34 +10,44 @@ happy_col = 0
 
 def row_check(n, m):
     global happy_row
-    for i in range(n):
-        cnt = 1
-        for j in range(1, n):
-            if grid[i][j] == grid[i][j-1]:
-                cnt += 1
-            else:
-                cnt = 1 
+    
+    if n > 1:
+        for i in range(n):
+            cnt = 1
+            for j in range(1, n):
+                if grid[i][j] == grid[i][j-1]:
+                    cnt += 1
+                else:
+                    cnt = 1 
 
-            if cnt >= m:
-                happy_row += 1
-                break
+                if cnt >= m:
+                    happy_row += 1
+                    break
+    else:
+        cnt = 1
+        if cnt >= m:
+            happy_row += 1
     
     return happy_row
 
 def col_check(n, m):
     global happy_col
-    for i in range(n):
-        cnt = 1
-        for j in range(1, n):
-            if grid[j][i] == grid[j-1][i]:
-                cnt += 1
-            else:
-                cnt = 1 
+    if n > 1:
+        for i in range(n):
+            cnt = 1
+            for j in range(1, n):
+                if grid[j][i] == grid[j-1][i]:
+                    cnt += 1
+                else:
+                    cnt = 1 
 
-            if cnt >= m:
-                happy_col += 1
-                break
-        
+                if cnt >= m:
+                    happy_col += 1
+                    break
+    else:
+        cnt = 1
+        if cnt >= m:
+            happy_col += 1   
     
     return happy_col
 
