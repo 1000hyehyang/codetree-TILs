d2bf9945c3eec04d635fefee372df687a7e65c8a
1000hyehyang <102294782+1000hyehyang@users.noreply.github.com>
2024-01-29 14:31:11 +0900

    Title: xor 결과 최대 만들기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240129/xor 결과 최대 만들기/max-of-xor.py
@@ -0,0 +1,41 @@
+n, m = tuple(map(int, input().split()))
+
+numbers = list(map(int, input().split()))
+answer = []
+
+max_num = 0
+
+def print_xor():
+    global max_num
+    lst = []
+    
+    for elem in answer:
+        lst.append(int(elem))
+    
+    for i in range(1, len(lst)):
+        temp = lst[0]
+        xor = temp ^ lst[i]
+        temp = xor
+
+    max_num = max(max_num, xor)
+    
+    return max_num
+
+
+
+def choose(cur_num, cnt):
+    if cur_num == n:
+        if cnt == m:
+            print_xor()
+        return
+
+    answer.append(numbers[cur_num])
+    choose(cur_num + 1, cnt + 1)
+    answer.pop()
+
+    choose(cur_num + 1, cnt)
+
+
+choose(0, 0)
+
+print(max_num)
